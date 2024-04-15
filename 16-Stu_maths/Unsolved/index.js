392be93e95115903ee49e7254559ba678d32557b
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-04-15 18:32:52 -0400

    initiating inquirer

--- 16-Stu_maths/Unsolved/index.js
+++ 16-Stu_maths/Unsolved/index.js
@@ -1,7 +1,29 @@
 // TODO: Import `maths.js`
+import { sum, difference, product, quotient } from "./maths";
+
 
 // TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
+const operation = process.argv[2];
+const numOne = parseInt(process.argv[3]);
+const numTwo = parseInt(process.argv[4]);
 
 // TODO: Create a `switch` statement that accepts an `operation` parameter
 // and each `case` uses the corresponding `maths` method
 // to perform each math operation on the two numbers, `numOne` and `numTwo`
+switch (operation) {
+    case "sum":
+        console.log(`${numOne} + ${numTwo} = ${sum(numOne, numTwo)}`);
+        break;
+    case "difference":
+        console.log(`${numOne} - ${numTwo} = ${difference(numOne, numTwo)}`);
+        break;
+    case "product":
+        console.log(`${numOne} * ${numTwo} = ${product(numOne, numTwo)}`);
+        break;
+    case "quotient":
+        console.log(`${numOne} / ${numTwo} = ${quotient(numOne, numTwo)}`);
+        break;
+    default:
+        console.log("Invalid operation!");
+}
+
